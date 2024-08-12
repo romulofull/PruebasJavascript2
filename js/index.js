aadf4f57d9d8320f26adc218d5df87ed6ab18af3
romulofull <romulo.castro@gmail.com>
2024-08-12 12:21:38 -0500

    Pruebas con un Arreglo

--- js/index.js
+++ js/index.js
@@ -43,4 +43,56 @@ document.body.append(buttonat);
 
 
 
+const namesa = ['ryan','jose','ramon']
 
+for (let i = 0; i < namesa.length; i++) {
+        const element = namesa[i];
+        console.log(element)
+    }
+
+const namesat = ['david','pedro','juan']
+namesat.forEach(function(nam) {
+    console.log(nam)
+})
+
+const namesate = ['jose','luis','james']
+namesate.map(function(nombres){
+console.log( nombres)
+
+})
+
+const namesatep = ['jose','luis','james']
+const nuevo = namesatep.map(function(nombres){
+return `hola ${nombres}`
+})
+
+console.log(namesatep)
+console.log(nuevo)
+
+
+const nuevaprueba = ['tatiana','laura','yolanda']
+const nameFound = nuevaprueba.find(function(nombre){
+if (nombre === 'yolanda') {
+return nombre
+}
+})
+
+console.log(nameFound)
+
+const nuevapruebas = ['riobamba','ambato','zamora']
+const namefiltrado = nuevapruebas.filter(function(nombres){
+if (nombres === 'ambato') {
+return nombres
+}
+})
+
+console.log(namefiltrado)
+
+const nuevapruebase = ['riobamba','ambato','zamora']
+const namefiltrados = nuevapruebase.filter(function(ns){
+if (ns !== 'ambato') {
+return ns
+}
+})
+
+console.log(namefiltrados)
