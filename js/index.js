ed19f642d0b9ed67c4bc8a77626880bb11c3d73b
romulofull <romulo.castro@gmail.com>
2024-08-11 22:32:45 -0500

    Pruebas

---
+++ js/index.js
@@ -0,0 +1,46 @@
+
+
+
+const background ='red'
+const color = 'blue'
+
+const button = document.createElement("button");
+button.innerText ="click me";
+button.style = `background: ${background}; color: ${color}`
+document.body.append(button);
+
+
+
+//const result = background + color
+
+const result = 'Estos son estilos: ${background} ${color}'
+console.log(result)
+
+
+
+const backgrounda ='red'
+const colora = 'yellow'
+const isauthorizeda = true;
+
+const buttona = document.createElement("buttona");
+button.innerText ="click me";
+button.style = `background: ${isauthorizeda ? 'blue' : 'red' }; color: ${colora}`
+document.body.append(buttona);
+
+
+
+//const result = background + color
+
+
+const backgroundat ='red'
+const colorat = 'yellow'
+const isauthorizedat = true;
+
+const buttonat = document.createElement("buttonat");
+buttonat.innerText ="click mee";
+buttonat.style = `background: ${isauthorizedat ? backgroundat : 'red' }; color: ${colorat}`
+document.body.append(buttonat);
+
+
+
+
